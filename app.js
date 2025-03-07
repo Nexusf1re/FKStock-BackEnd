072c7427f7b1791643117fb5e24f3af0c6b4c298
Matteus <matteussantos30@gmail.com>
2025-03-07 17:25:35 -0300

    Refactor: API working, server port adjust

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-import express from 'express';
 import itemRoutes from './routes/itemRoutes.js';
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.use(cors({}));
 
 app.use(express.json());
 app.use('/api', itemRoutes);
